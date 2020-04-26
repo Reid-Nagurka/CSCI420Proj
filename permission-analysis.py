5bafb63cb5ef0f86ffb4edb265aa495f7a430e15
Greg Garnhart <gegarnhart@email.wm.edu>
2020-04-26 15:06:59 -0400

    permission checker list

--- permission-analysis.py
+++ permission-analysis.py
@@ -4,3 +4,12 @@ def internet_checker(root):
         if(i.tag == "uses-permission" and i.attrib["{http://schemas.android.com/apk/res/android}name"] == "android.permission.INTERNET"):
             return True
     return False
+
+
+def get_requested_persmissions(root):
+    permission_list = []
+    for i in root:
+        if(i.tag == "uses-permission"):
+            perm = i.attrib["{http://schemas.android.com/apk/res/android}name"]
+            permission_list.append(perm)
+    return permission_list
